d00d82f98c073d97497530533a2d2dbc1eb94527
Enrique <enrique.turcott@cimav.edu.mx>
2012-03-09 16:27:45 -0700

    Add staff schedule

--- public/javascripts/staffs.js
+++ public/javascripts/staffs.js
@@ -218,4 +218,45 @@ $(".lab-practice-item").live("click", function() {
   }
 });
 
+$('#search-staff-schedule').live("click",function(){
+	url = location.pathname + '/horario/' + $('#staff_id').val()
+	var start_day 	= $('#start_date_start_year1994_3i').val();
+	var start_month = $('#start_date_start_year1994_2i').val();
+	var start_year	= $('#start_date_start_year1994_1i').val();
+	var end_day 	= $('#end_date_start_year1994_3i').val();
+	var end_month = $('#end_date_start_year1994_2i').val();
+	var end_year	= $('#end_date_start_year1994_1i').val();
+
+	var errors = 0;
+	if(end_year<start_year)
+	{
+			errors++;
+	}
+	
+	if(end_year==start_year)
+	{
+		if(end_month<start_month)
+		{
+			errors++;
+		}
+		
+		if(end_month==start_month)
+		{
+			if(end_day<start_day)
+			{
+				errors++;
+			}
+		}
+	}
+	
+	if(errors>0)
+	{
+		alert("La fecha de inicio no puede ser menor a la fecha de término");
+		return false;
+	}
+
+   $.get(url,{ start_day: start_day, start_month: start_month, start_year: start_year, end_day: end_day, end_month: end_month, end_year: end_year},function(html){
+				$('#staff-schedule-area').html(html);
+	});
+});
 
