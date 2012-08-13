f9f365a1309734c10130b925b18c75b8ca9a6d8b
Jonathan Hernández <ion@suavizado.com>
2012-08-13 13:10:38 -0600

    Script para correr en modo development desde localhost

---
+++ run-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export GOOGLE_KEY='435428651303-2q39a7rsgpdg62ckpkor7ph46ed0c3da.apps.googleusercontent.com'
+export GOOGLE_SECRET='0xgR6Om6HUUDjmQwhhw2SBc2'
+export RAILS_ENV='development'
+
+rails s
