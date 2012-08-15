9b039d50eead28ad674d7fa6f037e5ab27623412
Enrique Turcott <enrique.turcott@cimav.edu.mx>
2012-08-15 16:18:29 -0600

    Fix attr_accessible on program model

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class Program < ActiveRecord::Base
-  attr_accessible :id,:name,:level,:prefix,:description,:created_at,:updated_at,:terms_duration,:terms_qty
+  attr_accessible :id,:name,:level,:prefix,:description,:created_at,:updated_at,:terms_duration,:terms_qty,:terms_attributes
   has_many :documentation_file
   accepts_nested_attributes_for :documentation_file
 
