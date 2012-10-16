5d74fef233683df30c526aa867ce4b41ca7e1176
Enrique Turcott <enrique.turcott@cimav.edu.mx>
2012-10-16 11:21:39 -0600

    Authorizations details

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,24 +5,22 @@ class Ability
     user ||= User.new
     if user.access == User::MANAGER
       if user.program_type == Program::P_PROPEDEUTICOS
-        can :live_search, :all
-        can :manage, Student
-        can :manage, Program
-        can :manage, Internship
+        can :manage, [Student,Program,Internship]
       else
         can :manage, :all
       end
     end 
 
     if user.access == User::ADMINISTRATOR
-        can :live_search, :all
-        can [:read, :update, :files, :upload_file, :delete_file, :upload_image, :change_image, :file], Student
-        can [:read, :update], User
-        can :manage, [Staff, Program, Internship, Institution, Classroom, Laboratory, Department]
+        can :manage, [Student ,Staff, Program, Internship, Institution, Classroom, Laboratory, Department]
+        can :manage, User
+        cannot [:create,:destroy,:update], User
+        cannot [:create,:destroy], Student
     else
       can :live_search, :all  ## for default read all live_search
       if user.access == User::OPERATOR
-        can [:read, :update], Student
+        can :manage, Student
+        cannot [:create,:destroy], Student
         can :manage, Internship
       end
     end
