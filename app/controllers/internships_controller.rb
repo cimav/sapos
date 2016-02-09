bd4d9bed22ea286a0e42226b0cb72152e9c091e3
Enrique <enrique.turcott@cimav.edu.mx>
2016-02-08 17:32:42 -0700

    Fix internship applicant format

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -455,6 +455,9 @@ class InternshipsController < ApplicationController
   end
 
   def applicant_create
+    @internship = Internship.find(2315)
+    generate_applicant_document(@internship)
+=begin
     flash = {}
     @internship = Internship.new(params[:internship])
     @internship.status=3
@@ -479,8 +482,8 @@ class InternshipsController < ApplicationController
 
       ActivityLog.new({:user_id=>0,:activity=>"{:internship_id=>#{@internship.id},:activity=>'El usuario hace una solicitud por internet'}"}).save
       @uri = generate_applicant_document(@internship)
-      send_mail(@internship,@uri,1,nil)
-      send_mail(@internship,@uri,2,nil)
+      #send_mail(@internship,@uri,1,nil)
+      #send_mail(@internship,@uri,2,nil)
 
       respond_with do |format|
         format.html do
@@ -510,6 +513,7 @@ class InternshipsController < ApplicationController
         end
       end
     end
+=end
   end#applicant_create
 
   def applicant_file
@@ -571,9 +575,11 @@ class InternshipsController < ApplicationController
     @r_root  = Rails.root.to_s
     filename = "private/files/internships/#{i.id}"
     FileUtils.mkdir_p(filename) unless File.directory?(filename)
-    template = "#{@r_root}/private/prawn_templates/form_reg_estudiantes_externos.pdf"
-    Prawn::Document.generate("#{filename}/registro.pdf", :template => template)
-    pdf = Prawn::Document.new(:template => template)
+    template = "#{@r_root}/private/prawn_templates/form_reg_estudiantes_externos.png"
+    pdf = Prawn::Document.new(:background => template, :background_scale=>0.36, :right_margin=>20)
+    #Prawn::Document.generate("#{filename}/registro.pdf", :template => template)
+    #pdf = Prawn::Document.new(:template => template)
+   # pdf = Prawn::Document.new(:background=>template)
     pdf.draw_text i.full_name,  :at=>[60,567], :size=>10
     pdf.draw_text @genero,      :at=>[49,554], :size=>10
     pdf.draw_text @birth,       :at=>[117,541], :size=>10
@@ -585,6 +591,7 @@ class InternshipsController < ApplicationController
     pdf.draw_text @smedico,     :at=>[175,462], :size=>10
     pdf.draw_text @nsmedico,    :at=>[152,448], :size=>10
     pdf.draw_text @contacto,    :at=>[160,435], :size=>10
+=begin
     pdf.render_file "#{filename}/registro.pdf"
 
     token = Token.new
@@ -596,6 +603,8 @@ class InternshipsController < ApplicationController
     token.save
 
     return "/internados/aspirante/#{i.id}/formato/#{token.token}"
+=end
+    send_data pdf.render, type: "application/pdf", disposition: "inline"
   end
 
   def send_simple_mail(to,subject,content)
