be3ef0da5dd869e14cd1d1018e99304d369c619b
Enrique <enrique.turcott@cimav.edu.mx>
2016-10-10 12:14:22 -0600

    Remove config custom variables from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Sapos::Application.initialize!
 
-# Set config custom variables
-Sapos::Application.config.admin_email=""
