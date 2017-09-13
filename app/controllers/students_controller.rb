a0648ded4c1cd074e2affacd9bb9232175ccbda3
Geovany Gameros <geovanygameros@gmail.com>
2017-09-13 11:13:14 -0600

    Se cambia la firma de Emilio por la de Duarte en constancias

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2158,7 +2158,7 @@ class StudentsController < ApplicationController
     y    = 212
     h    = 45
     x    = x_right_top - w - 2
-    text = "Lic. Emilio Pascual Domínguez Lechuga\nCoordinación de Estudios de Posgrado"
+    text = "Dr. José Alberto Duarte Moller\nDirector Académico"
     pdf.text_box text, :at=>[x,y], :size=>size, :width=>w,:height=>h, :align=>:right, :valign=>:top
      
 
