6d43c51ff4cf4495260f14c81e4fedc56ad863bb
Geovany Gameros <geovanygameros@gmail.com>
2018-02-01 11:34:53 -0700

    Se corrige bug al inicializar evaluación docente

--- app/models/term_course_student.rb
+++ app/models/term_course_student.rb
@@ -1,13 +1,11 @@
 # coding: utf-8
 class TermCourseStudent < ActiveRecord::Base
-  attr_accessible :id,:term_course_id,:term_student_id,:grade,:status,:created_at,:updated_at
+  attr_accessible :id,:term_course_id,:term_student_id,:grade,:status,:created_at,:updated_at, :teacher_evaluation
 
   default_scope joins(:term_student=>[:student]).where('students.deleted=?',0).readonly(false)
 
   # inicializa el term_course_student con la evaluación sin contestar
-  before_create do
-    self.teacher_evaluation = false
-  end
+  after_create :init_teacher_evaluation
 
   belongs_to :term_course
   belongs_to :term_student
@@ -22,5 +20,10 @@ class TermCourseStudent < ActiveRecord::Base
 
   def status_type
     STATUS[status]
+  end
+
+  def init_teacher_evaluation
+    self.teacher_evaluation = false
+    self.save
   end
 end
