c034b1cc4de0e51f52597db5a5bb40a4f4682975
Jonathan Hernández <jonathan.hernandez@gmail.com>
2019-08-05 12:29:41 -0600

    Quitar file

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,7 @@ class StudentsController < ApplicationController
   REALM = "Students"
   PASS  = Digest::MD5.hexdigest(["dap",REALM,"53cr3t"].join(":"))
   USERS = {"u1"=>PASS}
-  before_filter :auth_required,:except=>[:student_exists, :file]
+  before_filter :auth_required,:except=>[:student_exists]
 
   before_filter :set_current_user
   respond_to :html, :xml, :json, :csv
